e296293e69074900292384c7f0e55dd8f90e2bfd
Leno <akrachunov@gmail.com>
2025-11-25 20:09:10 +0000

    Add langgraph application and update ignore file.

---
+++ backend/agents/src/agent/simple_agent.py
@@ -0,0 +1,48 @@
+from langchain.chat_models import init_chat_model
+from langchain.messages import SystemMessage
+from langgraph.graph import START, MessagesState, StateGraph
+
+model = init_chat_model(
+    "gpt-4o",
+    temperature=0
+)
+
+
+def llm_call(state: dict):
+    return {
+        "messages": [
+            model.invoke(
+                [
+                    SystemMessage(
+                        content="You are a helpful assistant tasked with performing arithmetic on a set of inputs."
+                    )
+                ]
+                + state["messages"]
+            )
+        ]
+    }
+
+
+# Build workflow
+agent_builder = StateGraph(MessagesState)
+
+# Add nodes
+agent_builder.add_node("llm_call", llm_call)
+
+# Add edges to connect nodes
+agent_builder.add_edge(START, "llm_call")
+
+# Compile the agent
+agent = agent_builder.compile()
+
+
+# from IPython.display import Image, display
+# # Show the agent
+# display(Image(agent.get_graph(xray=True).draw_mermaid_png()))
+
+# # Invoke
+# from langchain.messages import HumanMessage
+# messages = [HumanMessage(content="Add 3 and 4.")]
+# messages = agent.invoke({"messages": messages})
+# for m in messages["messages"]:
+#     m.pretty_print()

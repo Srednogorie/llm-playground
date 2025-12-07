1e155e72bb6f1ce9c8fcc363fa9525b99a9987bd
Alexander Krachunov <akrachunov@gmail.com>
2025-12-07 21:15:13 +0000

    Temporary remove dynamic agent selection.

--- agents/src/agent/simple_agent.py
+++ agents/src/agent/simple_agent.py
@@ -2,40 +2,66 @@ from dataclasses import dataclass
 
 from langchain.chat_models import init_chat_model
 from langchain.messages import SystemMessage
+from langchain_anthropic import ChatAnthropic
+from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI
 from langgraph.graph import START, MessagesState, StateGraph
 from langgraph.runtime import Runtime
 
 
 @dataclass
 class ContextSchema:
-    model: str
-    temperature: float
-    max_tokens: int
+    model: str = "gpt-4.1-nano"
+    temperature: float = 0.7
+    max_tokens: int = 1024
 
 
-model = init_chat_model()
+model = init_chat_model(configurable_fields="any")
 
 
 def llm_call(state: dict, runtime: Runtime[ContextSchema]):
-    return {
-        "messages": [
-            model.invoke(
-                [
-                    SystemMessage(
-                        content="You are a helpful assistant tasked with performing arithmetic on a set of inputs."
-                    )
-                ]
-                + state["messages"],
-                config={
-                    "configurable": {
-                        "model": runtime.context.model,
-                        "temperature": runtime.context.temperature,
-                        "max_tokens": runtime.context.max_tokens
+    # if runtime.context.model in ["gpt-4.1-nano", "claude-3-haiku-20240307"]:
+    if True:
+        return {
+            "messages": [
+                model.invoke(
+                    [
+                        SystemMessage(
+                            content="You are a helpful assistant tasked with performing arithmetic on a set of inputs."
+                        )
+                    ]
+                    + state["messages"],
+                    config={
+                        "configurable": {
+                            "model": "gpt-4.1-nano",
+                            # "temperature": runtime.context.temperature,
+                            # "max_tokens": runtime.context.max_tokens
+                        }
                     }
-                }
-            )
-        ]
-    }
+                )
+            ]
+        }
+    elif runtime.context.model == "ollama:gemma3:1b":
+        ollama_model = ChatOllama(
+            model="gemma3:1b",
+            temperature=runtime.context.temperature,
+            num_predict=runtime.context.max_tokens,
+            base_url="http://172.20.0.1:11434"
+        )
+        
+        return {
+            "messages": [
+                ollama_model.invoke(
+                    [
+                        SystemMessage(
+                            content="You are a helpful assistant tasked with performing arithmetic on a set of inputs."
+                        )
+                    ]
+                    + state["messages"],
+                )
+            ]
+        }
+    
 
 
 # Build workflow
